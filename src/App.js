4df38e37a480cfa47f3734cbd51529c486f2b9da
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2022-09-24 21:35:20 -0500

    added favicon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className=' App container'>
+    <div className=' App'>
       <div className='Frame container'>
         <InputCity defaultCity='New York' />
       </div>
